8db13d57c3e8eaa39e646864159985775c5e00c9
mzeecob <reshimwe2025@gmail.com>
2020-05-25 17:30:06 +0200

    course model

--- Backend/src/main/java/com/example/springbootwithreact/Course.java
+++ Backend/src/main/java/com/example/springbootwithreact/Course.java
@@ -1,7 +1,54 @@
 package com.example.springbootwithreact;
 
+import java.util.Objects;
+
 public class Course {
     private long id;
     private String username;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        Course course = (Course) o;
+        return getId() == course.getId() &&
+                getUsername().equals(course.getUsername()) &&
+                getDescription().equals(course.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getDescription());
+    }
+
+    public Course(long id, String username, String description) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
