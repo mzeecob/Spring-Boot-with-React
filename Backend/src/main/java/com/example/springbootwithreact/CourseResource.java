76b3a533f1d168a3bdb48047e2979d0d46099acf
mzeecob <reshimwe2025@gmail.com>
2020-05-27 19:42:47 +0200

    accept cross origin sites

--- Backend/src/main/java/com/example/springbootwithreact/CourseResource.java
+++ Backend/src/main/java/com/example/springbootwithreact/CourseResource.java
@@ -1,12 +1,14 @@
 package com.example.springbootwithreact;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class CourseResource {
     @Autowired
