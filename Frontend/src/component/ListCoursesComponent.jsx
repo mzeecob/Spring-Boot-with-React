031705420144469b3a75c27969405944b44ef99a
mzeecob <reshimwe2025@gmail.com>
2020-05-27 18:31:15 +0200

    add list courses component

---
+++ Frontend/src/component/ListCoursesComponent.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class ListCoursesComponent extends Component{
+
+    render() {
+        return(
+            <div className= "container">
+                <h3> All courses</h3>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Learn Full stack with Spring Boot and Angular</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default ListCoursesComponent
